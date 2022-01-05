9334196b0aff48030930d520c4365669b832bc5f
AaronRSG <aaronsgamble@gmail.com>
2022-01-05 12:07:21 +0000

    Rearranging bootstrap data

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -25,30 +25,32 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        Publisher penguin = new Publisher("Penguin", "Iceberg Street", "South Pole", "Antartica", "0000");
+
         Author brandon = new Author("Brandon", "Sanderson");
         Book twk = new Book("The Way of Kings", "9780765376671");
         brandon.getBooks().add(twk);
         twk.getAuthors().add(brandon);
+        penguin.getBooks().add(twk);
 
         authorRepository.save(brandon);
         bookRepository.save(twk);
+        publisherRepository.save(penguin);
 
         Author darren = new Author("Darren", "Shan");
         Book cdf = new Book("Cirque Du Freak", "9780316042963");
         darren.getBooks().add(cdf);
         cdf.getAuthors().add(darren);
+        penguin.getBooks().add(cdf);
 
         authorRepository.save(darren);
         bookRepository.save(cdf);
+        publisherRepository.save(penguin);
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepository.count());
-
-        Publisher penguin = new Publisher("Penguin", "Iceberg Street", "South Pole", "Antartica", "0000");
-        publisherRepository.save(penguin);
-
         System.out.println("Number of Publishers: " + publisherRepository.count());
-        System.out.print(penguin.toString());
+        System.out.print("Publisher Number of Books: " + penguin.getBooks().size());
     }
 
     
